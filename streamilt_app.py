1f528e8c3593290df8c4bf126caa4536ebeaa406
arunkumarja <141896999+arunkumarja@users.noreply.github.com>
2023-08-10 10:41:02 +0530

    Update streamilt_app.py
    fixfypo

--- streamilt_app.py
+++ streamilt_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title('My parents new health diner')
 
+streamlit.header('Breakfast menu')
+streamlit.text('omega 3 & blueberry oatemai')
+streamlit.text('omelt & offoil')
+streamlit.text('hard-boiled')
