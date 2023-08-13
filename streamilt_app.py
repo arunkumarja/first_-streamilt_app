f114f825137966c6c24e951d066f032b60ebc4a5
arunkumarja <141896999+arunkumarja@users.noreply.github.com>
2023-08-13 08:44:30 +0530

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -19,7 +19,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)   
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Orange")
 streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
@@ -29,8 +29,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Orange')
-streamlit.write('The user entered ', fruit_choice)
 
 import snowflake.connector
 
