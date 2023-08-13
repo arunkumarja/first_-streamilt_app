0aa301f8acb87fd9be43d79b6acee5c9ba17a9d7
arunkumarja <141896999+arunkumarja@users.noreply.github.com>
2023-08-13 08:58:04 +0530

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -29,6 +29,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/apple")
+streamlit.text(fruityvice_response)
 
 import snowflake.connector
 
